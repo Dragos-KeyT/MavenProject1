5fa63f2bbde6be4f6259632780bf5cca4f926101
Dragos Tanta <dragos.tanta@gmail.com>
2024-05-10 16:54:21 +0300

    curs 20 jumatate

--- src/main/java/curs19/SeleniumLocators.java
+++ src/main/java/curs19/SeleniumLocators.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import selenium.utils.BaseTest;
@@ -62,8 +63,40 @@ public class SeleniumLocators extends BaseTest{
 		
 		WebElement price = driver.findElement(By.className("price"));
 		System.out.println(price.getText());
-		assertTrue(price.getText().contains("18.50"));
+		assertTrue(price.getText().contains("18.49"));
 		
 	}
+	@Test(priority=5)
+	public void idLocator() {
+		
+		WebElement reviewTab =  driver.findElement(By.id("tab-title-reviews"));
+		reviewTab.click();
+		Actions action = new Actions(driver);
+		action.scrollByAmount(0, 500).perform();
+		WebElement commentBox =  driver.findElement(By.id("comment"));
+		assertTrue(commentBox.isDisplayed());
+	}
 	
+	@Test(priority=6)
+	public void nameLocator() throws InterruptedException {
+		WebElement commentBox =  driver.findElement(By.name("comment"));
+		commentBox.sendKeys("Super mesaj!");
+		Thread.sleep(3000);
+		commentBox.clear();//sterge valoarea prezenta intr-un input sau textarea field
+		commentBox.sendKeys("Alt Super mesaj!");
+	}
+	
+	
+	@Test(priority=7)
+	public void cssSelectorLocator() {
+		driver.findElement(By.cssSelector("input[id='author']")).sendKeys("John Doe");
+	}
+	
+	@Test(priority=8)
+	public void xpathLocator() {
+		
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("test@test.com");
+		
+		
+	}
 }
